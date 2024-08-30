43de5cbb602343febbededce403342a4c534dbf8
botondberde <botond_berde@icloud.com>
2024-08-30 20:36:08 +0100

    add req

--- code_to_text.sh
+++ code_to_text.sh
@@ -7,7 +7,7 @@ output_file="file_contents.txt"
 > "$output_file"
 
 # Exclude the output file as well as the other criteria
-find . -type f ! -name requirements.txt ! -path '*/node_modules/*' ! -name '.venv' ! -name "*.lock" ! -name '*.json' ! -path '*/.*' ! -path './.*' ! -name "*.md" ! -name "*.svg" ! -path "./${output_file}" | while read file; do
+find . -type f ! -name requirements.txt ! -path '*/node_modules/*' ! -path '*/dist/*' ! -path '*/build/*' ! -path '*/venv/*' ! -path '*/.venv/*' ! -name "*.lock" ! -name "*.sh" ! -name '*.json' ! -path '*/.*' ! -path './.*' ! -name "*.md" ! -name "*.svg" ! -path "./${output_file}" | while read file; do
     if [[ -f "$file" ]] && ! file --mime "$file" | grep -qE 'binary|executable'; then
         echo "Path: $file" >> "$output_file"
         cat "$file" >> "$output_file"
